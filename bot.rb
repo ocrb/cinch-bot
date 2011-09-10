bd5b19a729d6dff08c340d943972d2ff6505681f
Brendon Murphy <xternal1+github@gmail.com>
2011-09-10 10:22:09 -0700

    Added cinch-fortune

--- bot.rb
+++ bot.rb
@@ -1,6 +1,7 @@
 require "cinch"
 require "cinch/plugins/memo"
 require "cinch/plugins/last_seen"
+require "cinch/plugins/fortune"
 
 Cinch::Plugins::Memo::Base.configure do |c|
   c.store   = :redis          # data store
@@ -13,7 +14,11 @@ bot = Cinch::Bot.new do
     c.server           = "irc.freenode.net"
     c.nick             = "ocruby"
     c.channels         = ["#ocruby"]
-    c.plugins.plugins  = [Cinch::Plugins::Memo::Base, Cinch::Plugins::LastSeen::Base]
+    c.plugins.plugins  = [
+      Cinch::Plugins::Memo::Base,
+      Cinch::Plugins::LastSeen::Base,
+      Cinch::Plugins::Fortune
+    ]
   end
 
 end
