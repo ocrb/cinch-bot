df639c70778aeeff6b21a3bdcf58c57789b24726
Justin Beltran <justinvbeltran@gmail.com>
2011-09-15 21:23:30 -0700

    Added Meetup API call

--- bot.rb
+++ bot.rb
@@ -2,6 +2,37 @@ require "cinch"
 require "cinch/plugins/memo"
 require "cinch/plugins/last_seen"
 require "cinch/plugins/fortune"
+require "net/http"
+require "httparty"
+
+class Meetup
+  include Cinch::Plugin
+  include HTTParty
+
+  match "fetchtopic"
+
+  def execute(m)
+    m.reply "Setting topic..." 
+    sm.channel.topic = set_topic
+  end
+
+  timer 1800, :method => :timed
+  def timed
+    Channel("#ocruby").topic = set_topic
+  end
+
+  def next_meetup
+    resp = self.class.get "https://api.meetup.com/2/events?key=347072721a3c723a747c2467147728&sign=true&group_urlname=ocruby&page=20"
+    resp["results"].first
+  end
+
+  def set_topic
+    data = next_meetup
+    time = (Time.at data['time'].to_i / 1000).strftime("%b %d, %Y %I:%M%p")
+     "The next event, #{data['name']} @ #{time}. See #{data['event_url']} for more details." 
+  end
+end
+
 
 Cinch::Plugins::Memo::Base.configure do |c|
   c.store   = :redis          # data store
@@ -17,7 +48,8 @@ bot = Cinch::Bot.new do
     c.plugins.plugins  = [
       Cinch::Plugins::Memo::Base,
       Cinch::Plugins::LastSeen::Base,
-      Cinch::Plugins::Fortune
+      Cinch::Plugins::Fortune,
+      ::Meetup
     ]
   end
 
