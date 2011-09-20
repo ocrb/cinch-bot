a3afa2818f7b0e364c96d3f8f8ef23d5c9056345
Justin Beltran <justinvbeltran@gmail.com>
2011-09-19 20:30:38 -0700

    Fixing the text for displaying next topic

--- bot.rb
+++ bot.rb
@@ -22,14 +22,14 @@ class Meetup
   end
 
   def next_meetup
-    resp = self.class.get "https://api.meetup.com/2/events?key=347072721a3c723a747c2467147728&sign=true&group_urlname=ocruby&page=1"
+    resp = self.class.get "https://api.meetup.com/2/events?key=347072721a3c723a747c2467147728&sign=true&group_urlname=ocruby"
     resp["results"].first
   end
 
   def generated_topic
     data = next_meetup
     time = (Time.at data['time'].to_i / 1000).strftime("%b %d, %Y %I:%M%p")
-     "The next event, #{data['name']} @ #{time}. See #{data['event_url']} for more details." 
+     "Next event: #{data['name']} @ #{time}. See #{data['event_url']} for more details." 
   end
 end
 
