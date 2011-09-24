a72b269d7240e76dce1283f434e9e89fc687b5bd
Justin Beltran <justinvbeltran@gmail.com>
2011-09-24 13:54:55 -0700

    Add meetup to before block

--- spec/plugins/meetup_spec.rb
+++ spec/plugins/meetup_spec.rb
@@ -5,13 +5,13 @@ require File.expand_path('../../../plugins/meetup', __FILE__)
 describe Meetup do
   before do
     Meetup.stubs(:__register_with_bot).with(any_parameters).returns(true)
+    @meetup = Meetup.new(Object.new)
   end
 
   it "should get generated topic based on next meetup" do
-    meetup = Meetup.new(Object.new)
     next_meetup = { 'time' => '1817348000000', 'name' => 'NAME', 'event_url' => 'EVENT_URL'}
-    meetup.stubs(:next_meetup).returns(next_meetup)  
-    meetup.generated_topic.must_equal "Next event: NAME @ Aug 03, 2027 07:53PM. See EVENT_URL for more details." 
+    @meetup.stubs(:next_meetup).returns(next_meetup)  
+    @meetup.generated_topic.must_equal "Next event: NAME @ Aug 03, 2027 07:53PM. See EVENT_URL for more details." 
   end
 
 end
